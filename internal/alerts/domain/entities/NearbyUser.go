63f5bb2e76c00d2b8fe5d0e8e3c284ec3a635990
Alexia <233380@ids.upchiapas.edu.mx>
2026-03-12 04:53:51 -0600

    enviar ubicación en alerta

--- internal/alerts/domain/entities/NearbyUser.go
+++ internal/alerts/domain/entities/NearbyUser.go
@@ -5,8 +5,10 @@ type UserEntry struct {
 }
 
 type AlertPayload struct {
-	SenderID    int         `json:"sender_id"`
-	SenderName  string      `json:"sender_name"`
-	UsersFamily []UserEntry `json:"users_family"`
+	SenderID     int         `json:"sender_id"`
+	SenderName   string      `json:"sender_name"`
+	Latitude     float64     `json:"latitude"`
+	Longitude    float64     `json:"longitude"`
+	UsersFamily  []UserEntry `json:"users_family"`
 	UsersNetwork []UserEntry `json:"users_network"`
 }
