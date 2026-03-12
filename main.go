ad41f0bb73b68c6536da4d57d5fb81a23d8155c2
Alexia <233380@ids.upchiapas.edu.mx>
2026-03-12 03:27:14 -0600

    cambio de prueba

--- main.go
+++ main.go
@@ -4,18 +4,19 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"github.com/joho/godotenv"
 	"ws-server/internal/alerts/application"
 	"ws-server/internal/alerts/infrastructure"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	    err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error cargando .env")
-    }
-	
-    jwtSecret := os.Getenv("JWT_SECRET")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error cargando .env")
+	}
+
+	jwtSecret := os.Getenv("JWT_SECRET")
 
 	hub := infrastructure.NewHub()
 	processAlertUC := application.NewProcessAlertUseCase(hub)
@@ -24,6 +25,6 @@ func main() {
 	mux := http.NewServeMux()
 	infrastructure.RegisterRoutes(mux, alertHandler)
 
-	log.Println("Server running on :8081")
+	log.Println("Server running on :8081 ...")
 	log.Fatal(http.ListenAndServe(":8081", mux))
 }
